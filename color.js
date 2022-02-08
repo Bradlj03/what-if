daf6719620033e7c015bc5157c138613dbf7ba11
Jason Q Bradley <98352141+Bradlj03@users.noreply.github.com>
2022-02-08 12:09:20 -0500

    Node files addes

---
+++ color.js
@@ -0,0 +1,11 @@
+////const prompt = require("prompt-sync")();
+
+//let option = Number(prompt("Enter 1 for color contruction");
+
+
+//if(otion ===1){
+
+    // color contrction code
+//}
+
+    // color contrction code
